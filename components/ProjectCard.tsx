ff4c0701ca66bfd25242637b64966d35416d79b6
Damon Cai <20635117+oneiriac@users.noreply.github.com>
2020-05-17 00:14:47 +1000

    Refine hover styles for ProjectCard

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,16 +15,33 @@ const ProjectCard: React.FunctionComponent<ProjectProps> = ({
         }
 
         .project-card {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
           box-shadow: 0 5px 10px rgba(154, 160, 185, 0.05),
             0 15px 40px rgba(67, 107, 227, 0.2);
-          background-color: rgba(67, 107, 227, 0.85);
-          color: #232323;
+          background-color: rgba(67, 107, 227, 0.65);
+          color: #34384d;
           padding: 1rem;
-          transition: 0.2s;
+          min-height: 10rem;
+          transition: background-color 0.35s;
         }
 
-        .project-card:hover {
+        .project-card h3 {
+          margin-top: 0;
+          transition: color 0.35s;
+        }
+
+        .project-card:hover,
+        .project-card:focus,
+        .project-card:active {
           background-color: rgba(67, 107, 227, 1);
+        }
+
+        .project-card:hover,
+        .project-card:focus,
+        .project-card:active h3 {
+          color: #dedede;
         }
 
         .tech-icons-row {
@@ -46,7 +63,11 @@ const ProjectCard: React.FunctionComponent<ProjectProps> = ({
         <div className="tech-icons-row">
           {Array.isArray(techsUsed) &&
             techsUsed.map((tech) => (
-              <img src={tech.icon.url} alt={tech.icon.alt ?? undefined} />
+              <img
+                key={tech.name}
+                src={tech.icon.url}
+                alt={tech.icon.alt ?? undefined}
+              />
             ))}
         </div>
       </div>
